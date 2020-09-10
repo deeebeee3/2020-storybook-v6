5c8d57fc4bc5cc45e6ef37b8d077375d4951f635
Deepak Bhari <deepak.bhari@lifts.to>
2020-09-10 13:45:17 +0100

    Decorators - Method 2 - Write once by adding a property to default exports component to center align them

--- src/components/button/Button.stories.js
+++ src/components/button/Button.stories.js
@@ -9,29 +9,14 @@ export default {
   args: {
     children: "Setting args at Component Level",
   },
+  decorators: [(story) => <Center>{story()}</Center>],
 };
 
 //renders actual components
-export const Primary = () => (
-  <Center>
-    <Button variant="primary">Primary</Button>
-  </Center>
-);
-export const Secondary = () => (
-  <Center>
-    <Button variant="secondary">Secondary</Button>
-  </Center>
-);
-export const Success = () => (
-  <Center>
-    <Button variant="success">Success</Button>
-  </Center>
-);
-export const Danger = () => (
-  <Center>
-    <Button variant="danger">Danger</Button>
-  </Center>
-);
+export const Primary = () => <Button variant="primary">Primary</Button>;
+export const Secondary = () => <Button variant="secondary">Secondary</Button>;
+export const Success = () => <Button variant="success">Success</Button>;
+export const Danger = () => <Button variant="danger">Danger</Button>;
 
 const Template = (args) => <Button {...args} />;
 
