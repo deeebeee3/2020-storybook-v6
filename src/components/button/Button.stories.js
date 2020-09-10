5b70c19724a32d480aed5c87e8e5dead6a1b7002
Deepak Bhari <deepak.bhari@lifts.to>
2020-09-10 13:41:45 +0100

    Decorators - Method 1 - wrap components with utility component to center align them

--- src/components/button/Button.stories.js
+++ src/components/button/Button.stories.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "./Button";
+import Center from "../Center/Center";
 
 //adds entry in side map
 export default {
@@ -11,10 +12,26 @@ export default {
 };
 
 //renders actual components
-export const Primary = () => <Button variant="primary">Primary</Button>;
-export const Secondary = () => <Button variant="secondary">Secondary</Button>;
-export const Success = () => <Button variant="success">Success</Button>;
-export const Danger = () => <Button variant="danger">Danger</Button>;
+export const Primary = () => (
+  <Center>
+    <Button variant="primary">Primary</Button>
+  </Center>
+);
+export const Secondary = () => (
+  <Center>
+    <Button variant="secondary">Secondary</Button>
+  </Center>
+);
+export const Success = () => (
+  <Center>
+    <Button variant="success">Success</Button>
+  </Center>
+);
+export const Danger = () => (
+  <Center>
+    <Button variant="danger">Danger</Button>
+  </Center>
+);
 
 const Template = (args) => <Button {...args} />;
 
