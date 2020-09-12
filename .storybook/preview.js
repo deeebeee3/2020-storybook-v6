d7cf3723116563ddae6ff57ed3eae6ec64ea1879
Deepak Bhari <deepak.bhari@lifts.to>
2020-09-12 17:36:45 +0100

    Decorators and Theming in Storybook v6

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { addDecorator } from "@storybook/react";
 import Center from "../src/components/Center/Center";
+import { ThemeProvider, theme, CSSReset, Box } from "@chakra-ui/core";
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -12,4 +13,16 @@ export const parameters = {
   },
 };
 
-addDecorator((story) => <Center>{story()}</Center>);
+// addDecorator((story) => <Center>{story()}</Center>);
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Box m="4">
+        <Story />
+      </Box>
+    </ThemeProvider>
+  ),
+  (Story) => <Center>{Story()}</Center>,
+];
